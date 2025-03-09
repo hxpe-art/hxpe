acc63c05afce04b8b50191dabddae78afb0ca50f
hxpe-art <hopebxcca@gmail.com>
2025-03-09 17:58:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const button = document.getElementById("getDetails");
 const details = document.getElementById("details");
+
 button.addEventListener("click", async () => {
   try {
-  } catch(err) {
-    console.error(err);
-    alert("error occured");
-  }
-});
 //request device
 const device = await navigator.bluetooth.requestDevice({
   optionalServices: ["battery_service", "device_information"],
@@ -49,5 +45,10 @@ promise.then(() => {
   ${infoValues.map((value) => `<li>${value}</li>`).join("")}
 </u1>
   `;
+});
+      } catch(err) {
+    console.error(err);
+    alert("error occured");
+  }
 });
   
