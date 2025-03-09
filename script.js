c21713460e299bf68c7b5b81e31688704c7a894b
hxpe-art <hopebxcca@gmail.com>
2025-03-09 17:24:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,35 @@ const device = await navigator.bluetooth.requestDevice({
   optionalServices: ["battery_service", "device_information"],
   acceptAllDevices: true,
 });
-//connect to gatt server
+//connect to gatt server, waiting for promise
 let deviceName = device.gatt.device.name;
 const server = await device.gatt.connect();
 //get service (bluetoooth thing?)
 const batteryService = await server.getPrimaryService("battery_service")
 const infoService = await server.getPrimaryService("device_information");
+//getting characteristics, promise
+const infoCharacterists = await infoService.getCharacteristics();
+console.log(infoCharacteristics)
+let infoValues = [];
+const promise = new Promise((resolve, reject) => {
+  infoCharacteristics.forEach(async (characteristic, index, array) => {
+    //returns a buffer?? used to represent a fixed-length sequence of bytes
+    const value = await characteristic.readValue();
+    console.log(new TextDecoder().decode(value));
+    //converts to string, sequence of numbers/letters
+    infoValues.push(new TextDecoder().decode(value));
+    if (index === array.length -1) resolve();
+  });
+});
+promise.then(() => {
+  //displaying everything
+  details.innerHTML ='
+    Device Name -${deviceName}<br />
+             Battery Level -${batteryPercent}%<br />
+  Device Information:
+<u1>
+  ${infoValues.map((value) => '<li>${value}</li>').join("")}
+<u1>
+  ';
+});
+  
